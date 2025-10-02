5c02a219a4464c47f595b664f96aa28734336097
alex101xela <alex@ex10.org>
2025-10-02 17:28:43 +0400

    [ts] Add transfer examples

--- typescript/src/api/transfer.ts
+++ typescript/src/api/transfer.ts
@@ -5,5 +5,5 @@ import { TransferResponseSchema } from './transfer.schema.ts'
 export const transfer = async (transfer: Transfer) => {
   const { data } = await axiosClient.post<unknown>('/api/v1/user/transfer', transfer)
 
-  return TransferResponseSchema.parse(data)
+  return TransferResponseSchema.parse(data).data
 }
