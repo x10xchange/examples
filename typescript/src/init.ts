0a776d29e2f5567045e7216d0f144788a0ea4ab4
alex101xela <alex@ex10.org>
2026-01-20 15:04:08 +0400

    Add "Ethics3606/extended-rust-sdk"

--- typescript/src/init.ts
+++ typescript/src/init.ts
@@ -22,8 +22,6 @@ export const init = async () => {
   setHost(process.env.API_HOST)
   setApiKey(process.env.API_KEY)
 
-  // FIXME: print env info
-
   return {
     apiKey: process.env.API_KEY,
     starkPrivateKey: process.env.STARK_PRIVATE_KEY,
